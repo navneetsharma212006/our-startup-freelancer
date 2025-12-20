7ef895ef829c73f02a72010aa006fbc694c822d6
ayushman1210 <ayushmangupta892@gmail.com>
2025-12-20 19:16:25 +0530

    changes

--- aeroai/src/routes/ans-evalrouter.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const evalute=express.Router();
-const {rank, evaluateanswer}=require('../controllers/evaluatecontroller');
-
-
-evalute.post('/evaluate-answer',evaluateanswer)
-evalute.get('/rank-candidates',rank)
-
-module.exports=evalute;
